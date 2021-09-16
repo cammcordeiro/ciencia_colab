e8bf1091aeebbe516ed554059c1cf6da81bbeb1d
Cesar Cordeiro <cammcordeiro@id.uff.br>
2021-09-16 19:21:16 -0300

    moved to another folder

---
+++ scripts/atividade2.R
@@ -0,0 +1,109 @@
+
+### Atividade 2 ###
+
+### Darwin Core
+
+## carregar pacotes
+library(tidyverse)
+library(taxize)
+
+## carregar dados
+
+# dados prontos
+iris <- read.csv("~/github/cursodados/data/iris_mod.csv", header = T)
+
+lapply(iris, unique)
+
+# dados dos participantes
+
+
+# checar taxa
+species <- iris %>% 
+  distinct(Species) %>% 
+  pull() %>% 
+  get_tsn() %>% 
+  data.frame() %>% 
+  bind_cols(iris %>% 
+              distinct(Species))
+
+
+### Manipulando os dados
+
+iris_1 <- iris %>% 
+  dplyr::mutate(eventID = paste(site, date, sep = "_"), # create indexing fields 
+                occurrenceID = paste(site, date, amostra, sep = "_")) %>% 
+  left_join(species %>% 
+              select(Species, uri)) %>% # add species unique identifier
+  dplyr::rename(decimalLongitude = lon, # rename fields according to DwC 
+                decimalLatitude = lat,
+                eventDate = date,
+                scientificName = Species,
+                scientificNameID = uri) %>% 
+  mutate(geodeticDatum = "WGS84", # and add complimentary fields
+         verbatimCoordinateSystem = "decimal degrees",
+         georeferenceProtocol = "Random coordinates obtained from Google Earth",
+         locality = "Gaspe Peninsula",
+         recordedBy = "Edgar Anderson",
+         taxonRank = "Species",
+         organismQuantityType = "individuals",
+         basisOfRecord = "Human observation")
+
+
+### criando o eventCore ###
+eventCore <- iris_1 %>% 
+  select(eventID, eventDate, decimalLongitude, decimalLatitude, locality, site,
+         geodeticDatum, verbatimCoordinateSystem, georeferenceProtocol) %>% 
+  distinct() 
+
+
+### criando o occurrence ###
+occurrences <- iris_1 %>% 
+  select(eventID, occurrenceID, scientificName, scientificNameID,
+         recordedBy, taxonRank, organismQuantityType, basisOfRecord) %>%
+  distinct() 
+
+
+## criando o measurementsOrFacts
+eMOF <- iris_1 %>% 
+  select(eventID, occurrenceID, recordedBy, Sepal.Length:Petal.Width) %>%  
+  pivot_longer(cols = Sepal.Length:Petal.Width,
+               names_to = "measurementType",
+               values_to = "measurementValue") %>% 
+  mutate(measurementUnit = "cm",
+         measurementType = plyr::mapvalues(measurementType,
+                                           from = c("Sepal.Length", "Sepal.Width", "Petal.Width", "Petal.Length"), 
+                                           to = c("sepal length", "sepal width", "petal width", "petal length")))
+
+### controle de qualidade
+
+# checar eventID
+setdiff(eventCore$eventID, occurrences$eventID)
+setdiff(eventCore$eventID, eMOF$eventID)
+setdiff(occurrences$eventID, eMOF$eventID)
+
+
+# checar NAs
+eMOF %>%
+  filter(is.na(eventID))
+
+
+occurrences %>%
+  filter(is.na(eventID))
+
+
+### salvar os dados
+
+# limpar memória
+rm(list = setdiff(ls(), c("eventCore", "occurrences", "eMOF")))
+
+# ler arquivos
+files <- list(eventCore, occurrences, eMOF) 
+data_names <- c("DF_eventCore","DF_occ","DF_eMOF")
+dir.create("Dwc_Files")
+
+# loop para criar arquivos *.csv
+for(i in 1:length(files)) {
+  path <- paste0(getwd(), "/", "DwC_Files")
+  write.csv(files[[i]], paste0(path, "/", data_names[i], ".csv"))
+}
+
